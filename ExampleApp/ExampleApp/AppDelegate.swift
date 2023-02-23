de37a3acf391c24f80e181c6b0cc5daf42885d93
Alvin He <alvinh@squareup.com>
2023-02-23 21:09:28 +1030

    Added example app

---
+++ ExampleApp/ExampleApp/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  ExampleApp
+//
+//  Created by Alvin He on 23/2/2023.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = ViewController(collectionViewLayout: .init())
+        window.makeKeyAndVisible()
+        self.window = window
+        return true
+    }
+}
+
---
+++ ExampleApp/ExampleApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ExampleApp
+//
+//  Created by Alvin He on 23/2/2023.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+

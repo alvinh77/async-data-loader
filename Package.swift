bb1f58c84ace33bb2ac8266106a96891019cf3c5
Alvin He <alvinh@squareup.com>
2023-04-01 16:05:47 +1030

    Update min support version to iOS15

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "AsyncDataLoader",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -21,9 +21,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AsyncDataLoader",
-            dependencies: []),
+            dependencies: []
+        ),
         .testTarget(
             name: "AsyncDataLoaderTests",
-            dependencies: ["AsyncDataLoader"]),
+            dependencies: ["AsyncDataLoader"]
+        ),
     ]
 )

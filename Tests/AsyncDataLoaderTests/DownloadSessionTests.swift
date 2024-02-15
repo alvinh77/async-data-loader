2466b3c7bb7eb78cb6fb6b5b09aae627a6e62096
Alvin <alvinh1223@gmail.com>
2024-02-15 14:16:48 +1100

    Added unit tests for DownloadSession

---
+++ Tests/AsyncDataLoaderTests/DownloadSessionTests.swift
@@ -0,0 +1,73 @@
+//
+//  DownloadSessionTests.swift
+//  AsyncDataLoader
+//
+//  Created by Alvin He on 14/2/2024.
+//
+
+import XCTest
+
+@testable import AsyncDataLoader
+
+final class DownloadSessionTests: XCTestCase {
+    func test_downloadStream_whenReceiveProgressAndFinish() async throws {
+        let url = try XCTUnwrap(URL(string: "https://www.test.com"))
+        let downloadSession = DownloadSession(
+            url: url,
+            serverSession: TestServerSession(),
+            dataProvider: { _ in Data(count: 10) }
+        )
+        let downloadStream = downloadSession.downloadStream
+        let urlSession = URLSession(configuration: .ephemeral)
+        let request = URLRequest(url: url)
+        let downloadTask = urlSession.downloadTask(with: request)
+        downloadSession.urlSession(
+            urlSession,
+            downloadTask: downloadTask,
+            didWriteData: 10,
+            totalBytesWritten: 15,
+            totalBytesExpectedToWrite: 30
+        )
+        downloadSession.urlSession(
+            urlSession,
+            downloadTask: downloadTask,
+            didFinishDownloadingTo: url
+        )
+        var statuses = [DataStatus]()
+        for try await status in downloadStream {
+            statuses.append(status)
+        }
+        
+        XCTAssertEqual(
+            statuses,
+            [.inProgress(0.5), .finished(Data(count: 10))]
+        )
+    }
+    
+    func test_downloadStream_whenFinishButCouldNotFindData() async throws {
+        let url = try XCTUnwrap(URL(string: "file:///Users"))
+        let downloadSession = DownloadSession(
+            url: url,
+            serverSession: TestServerSession()
+        )
+        let downloadStream = downloadSession.downloadStream
+        let urlSession = URLSession(configuration: .ephemeral)
+        let request = URLRequest(url: url)
+        let downloadTask = urlSession.downloadTask(with: request)
+        downloadSession.urlSession(
+            urlSession,
+            downloadTask: downloadTask,
+            didFinishDownloadingTo: url
+        )
+        var statuses = [DataStatus]()
+        do {
+            for try await status in downloadStream {
+                statuses.append(status)
+            }
+        } catch {
+            XCTAssertEqual(error as? DataLoaderError, .dataNotFound)
+        }
+        
+        XCTAssertEqual(statuses.count, 0)
+    }
+}
